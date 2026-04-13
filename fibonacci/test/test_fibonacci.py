d3bcaf65e80090d0aa446d2b04020cc58d09a8a5
Hristo Petkov <petkov.t.hristo@gmail.com>
2026-04-13 21:59:43 +0300

    test: add test cases for Fibonacci function

---
+++ fibonacci/test/test_fibonacci.py
@@ -0,0 +1,20 @@
+'''
+Unit tests for the Fibonacci function.
+
+This module uses the pytest framework to test the functionality of the Fibonacci class.
+'''
+
+import pytest
+from fibonacci.src.fibonacci import Fibonacci
+
+def test_fibonacci():
+    fib = Fibonacci()
+    expected_output = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    assert fib.fibonacci() == expected_output
+
+def test_print_fibonacci(capsys):
+    fib = Fibonacci()
+    fib.print_fibonacci()
+    captured = capsys.readouterr()
+    expected_output = "The first 10 Fibonacci numbers are: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]\n"
+    assert captured.out == expected_output
