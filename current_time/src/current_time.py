b4d22e8addb2003dbaf24a6bb304d11589c14584
Hristo Petkov <petkov.t.hristo@gmail.com>
2026-04-13 19:40:47 +0300

    refactor: update current_time.py to use datetime module for better time handling

--- current_time/src/current_time.py
+++ current_time/src/current_time.py
@@ -1,11 +1,11 @@
 '''
 This module provides a function to get the current time in a specific format.
 '''
-from datetime import datetime
+import datetime as dt
 
 def get_current_time() -> str:
     '''
     This function returns the current time in the format "HH:MM:SS.F".
     :return: A string representing the current time.
     '''
-    return datetime.now().strftime("%H:%M:%S.%f")
+    return dt.datetime.now().strftime("%H:%M:%S.%f")
