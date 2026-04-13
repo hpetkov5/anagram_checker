091e6d262eba3e43c6cda1ac80b19e79e03f647d
Hristo Petkov <petkov.t.hristo@gmail.com>
2026-04-13 22:18:33 +0300

    feat: add factorial function

---
+++ factorial/src/factorial.py
@@ -0,0 +1,18 @@
+'''
+This module contains functions to calculate the factorial of n iteratively and recursively.
+'''
+
+def factorial_iterative(number: int) -> int:
+    '''
+    Iterative factorial function which takes an integer and returns it factorial.
+    '''
+    fact = 1
+    for number in range(2, number + 1):
+        fact *= number
+    return fact
+
+def factorial_recursive(number: int) -> int:
+    '''
+    Recursive factorial function which takes an integer and returns it factorial.
+    '''
+    return 1 if number == 1 else number * (factorial_recursive(number -1))
